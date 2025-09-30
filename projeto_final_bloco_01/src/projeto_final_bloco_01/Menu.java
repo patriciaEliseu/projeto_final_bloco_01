e51caf8f60efe0f2c0101f75bcb5ea2a83bff75f
Patricia Eliseu <patyeliseu4@gmail.com>
2025-09-30 14:39:52 -0300

    add controller

--- projeto_final_bloco_01/src/projeto_final_bloco_01/Menu.java
+++ projeto_final_bloco_01/src/projeto_final_bloco_01/Menu.java
@@ -3,13 +3,17 @@ package projeto_final_bloco_01;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import projeto_final_bloco_01.controller.ProdutoController;
+import projeto_final_bloco_01.model.Bijuteria;
+import projeto_final_bloco_01.model.Decoracao;
+import projeto_final_bloco_01.model.Produtos;
 import projeto_final_bloco_01.util.Cores;
 
 public class Menu {
 	
 		private static final Scanner leia = new Scanner(System.in);
 
-		
+		private static final ProdutoController produtoController = new ProdutoController();
 
 		public static void main(String[] args) {
 
@@ -60,22 +64,22 @@ public class Menu {
 				case 1:
 					System.out.println(Cores.TEXT_BLACK + "Criar Produto\n\n");
 
-					
+					cadastrarProduto();
 
 					keyPress();
 					break;
 				case 2:
 					System.out.println(Cores.TEXT_BLACK + "Listar todos os Produtos\n\n");
 
-					
+					listarProdutos();
 
 					keyPress();
 					break;
 				case 3:
 					System.out.println(Cores.TEXT_BLACK + "Consultar dados dos Produtos - por ID\n\n");
 
+					procurarProdutoPorId();
 					
-
 					keyPress();
 					break;
 				case 4:
@@ -88,7 +92,7 @@ public class Menu {
 				case 5:
 					System.out.println(Cores.TEXT_BLACK + "Apagar o Produto\n\n");
 
-					
+					deletarProduto();
 
 					keyPress();
 					break;
@@ -115,5 +119,152 @@ public class Menu {
 			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar...");
 			leia.nextLine();
 		}
+		
+		private static void listarProdutos() {
+			produtoController.listarTodos();
+		}
+		
+		private static void cadastrarProduto() {
+			System.out.print("Digite a descrição do produto: ");
+			leia.skip("\\R");
+			String descricao = leia.nextLine();
+			
+			System.out.print("Digite o tipo do produto( 1 - Deco | 2 - Biju): ");
+			int tipo = leia.nextInt();
+			
+			System.out.print("Digite o Artesao do produto: ");
+			String artesao = leia.nextLine();
+			
+			System.out.print("Digite o preco do produto: ");
+			float preco = leia.nextFloat();
+			
+			switch (tipo) {
+			case 1 -> {
+				
+				produtoController
+						.cadastrar(new Decoracao (produtoController.gerarId(),  tipo, descricao,artesao, preco));
+			}
+			case 2 -> {
+				System.out.print("Digite o genero do material:  ");
+				String genero = leia.nextLine();
+				leia.nextLine();
+				produtoController.cadastrar(
+						new Bijuteria(produtoController.gerarId(), tipo, descricao, artesao, preco,  genero ));
+			}
+			default -> System.out.println(Cores.TEXT_RED + "Tipo de Produto inválido!" + Cores.TEXT_RESET);
+			}
+		}
+			
+		private static void procurarProdutoPorId() {
+			
+			System.out.print("Digite o Id do produto: ");
+			int id = leia.nextInt();
+			leia.nextLine();
+
+			produtoController.procurarPorId(id);
+			
+		}
+		
+		
+		private static void deletarProduto() {
+
+			System.out.print("Digite o id do produto: ");
+			int numero = leia.nextInt();
+			leia.nextLine();
+
+			Produtos produto = produtoController.buscarNaCollection(numero);
+
+			
+			if (produto != null) {
+
+				
+				System.out.print("\nTem certeza que deseja excluir este produto? (S/N): ");
+				String confirmacao = leia.nextLine();
+
+				if (confirmacao.equalsIgnoreCase("S")) {
+					produtoController.deletar(numero);
+				} else {
+					System.out.println("\nOperação cancelada!");
+				}
+
+			} else {
+				System.out.printf("\nO produto id %d não foi encontrado!", numero);
+			}
+		}
+
+		private static void atualizarProduto() {
+
+			System.out.print("Digite o id do produto: ");
+			int numero = leia.nextInt();
+			leia.nextLine();
+
+			
+			Produtos produto = produtoController.buscarNaCollection(numero);
+
+			
+			if (produto != null) {
+
+			
+				int tipo = produto.getTipo(); 
+				String descricao = produto.getDescricao();
+				String artesao = produto.getArtesao();
+				float preco = produto.getPreco();
+				
+
+				
+				System.out.printf(
+						"tipo de produto: %d\nDigite o novo tipo do produto. (Pressione ENTER para manter o valor atual): ",
+						tipo);
+				int entrada = leia.nextInt();
+
+				
+				tipo = entrada.isEmpty() ? tipo : entrada;
+
+				
+				System.out.printf(
+						"Descrição atuall: %s\nDigite a nova descriçao do produto. (Pressione ENTER para manter o valor atual): ",
+						descricao);
+				entrada = leia.nextLine();
+				descricao = entrada.isEmpty() ? descricao : entrada;
+
+				
+				
+
+				// Se a conta for do tipo Conta Corrente
+				switch (tipo) {
+				case 1 -> {
+
+				
+					
+					produtoController.atualizar(new Decoracao(numero, tipo, descricao,artesao, preco));
+
+				}
+				
+				case 2 -> {
+
+					String genero = ((Bijuteria) produto).getGenero();
+
+					
+					System.out.printf(
+							"Genero atual é: %s\nDigite o novo Gẽnero. (Pressione ENTER para manter o valor atual): ",
+							genero);
+					entrada = leia.nextLine();
+					genero = entrada.isEmpty() ? genero : entrada;
+					produtoController.atualizar(new Bijuteria(numero, tipo, descricao, artesao, preco,  genero));
+				}
+				
+				default -> System.out.println(Cores.TEXT_RED + "Tipo de Biju inválido!" + Cores.TEXT_RESET);
+				}
+
+			} else {
+				// Caso a conta não exista
+				System.out.printf("\nO produto de id %d não foi encontrado!", numero);
+			}
+
+		}
+
+		
+			
+
 
 }
---
+++ projeto_final_bloco_01/src/projeto_final_bloco_01/controller/ProdutoController.java
@@ -0,0 +1,89 @@
+package projeto_final_bloco_01.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import projeto_final_bloco_01.model.Produtos;
+import projeto_final_bloco_01.repository.ProdutoRepository;
+
+public class ProdutoController implements ProdutoRepository {
+
+	private List<Produtos> listaProdutos = new ArrayList<Produtos>();
+	int id = 0;
+
+	@Override
+	public void listarTodos() {
+		for (var produto : listaProdutos) {
+			produto.visualizarTipo();
+		}
+
+	}
+
+	@Override
+	public void cadastrar(Produtos produto) {
+		listaProdutos.add(produto);
+		System.out.println("\nConta cadastrada com sucesso!");
+
+	}
+
+	@Override
+	public void atualizar(Produtos produto) {
+		
+		var buscarProduto = buscarNaCollection(id);
+		
+		if(buscarProduto != null) {
+			listaProdutos.set(listaProdutos.indexOf(buscarProduto), produto);
+			System.out.printf("\nO Produto id: %d foi atualizado com sucesso!%n", produto.getId());
+		}else {
+			System.out.printf("\nO produto id: %d não foi encontrado!%n", produto.getId());
+		}		
+		
+	}
+
+	@Override
+	public void procurarPorId(int id) {
+		
+		var produto = buscarNaCollection(id);
+		
+		if(produto != null) {
+			produto.visualizarTipo();
+		} else {
+			System.out.printf("\nO produto id %d não foi encontrado!%n ", id);
+		}
+
+	}
+
+	@Override
+	public void deletar(int id) {
+		var produto = buscarNaCollection(id);
+		
+		if(produto != null) {
+			if (listaProdutos.remove(produto) == true) {
+				System.out.printf("\nO produto id %d foi deletado com sucesso!%n ", id);
+			}
+			
+		}else {
+			System.out.printf("\nO produto id %d não foi encontrado!%n ", id);
+		}
+
+	}
+
+	
+	
+	
+	
+	public int gerarId() {
+		return ++id;
+	}
+	
+	public Produtos buscarNaCollection(int id) {
+		for(var produto : listaProdutos) {
+			if(produto.getId() == id) {
+				return produto;
+			}
+		}
+		
+		return null;
+	}
+
+}
